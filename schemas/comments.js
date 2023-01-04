fa216499cd00b46e3020d966d17201124b8d7899
GithuiVictor <victorgithuim@gmail.com>
2023-01-04 16:44:03 +0300

    first commit

---
+++ schemas/comments.js
@@ -0,0 +1,17 @@
+export default {
+    name: 'comment',
+    title: 'Comment',
+    type: 'document',
+    fields: [
+        {
+            name: 'posted',
+            title: 'PostedBy',
+            type: 'postedBy'
+        },
+        {
+            name: 'comment',
+            title: 'Comment',
+            type: 'string'
+        }
+    ]
+}
--- schemas/index.js
+++ schemas/index.js
@@ -1,1 +1,13 @@
-export const schemaTypes = []
+import comments from "./comments";
+import pin from "./pin";
+import postedBy from "./postedBy";
+import save from "./save";
+import user from "./user";
+
+export const schemaTypes = [
+    user,
+    pin,
+    comments,
+    postedBy,
+    save
+]
